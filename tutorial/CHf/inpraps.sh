3049a663beb008be015d2fd9db0a93e88cd279b2
Dryctarth <xanshriekal@yahoo.com>
2025-10-22 21:36:16 -0500

    Add files via upload

---
+++ tutorial/CHf/inpraps.sh
@@ -0,0 +1,17 @@
+#SPHt_10
+els=(C Hf)
+LevMTP=16
+DFT_CFG=aflow_CHf.cfg
+URX_CFG=randspg_CHf.cfg
+REF_CFG=ref.cfg
+mindist=0.9
+maxdist=3.1
+cmpd_pth=.../PRAPs/tutorial/CHf
+chull_var=0.05
+relax_settings="--iteration-limit=100"
+basic_acc=false
+filter_trajectories=false
+filter_volumes=false
+volume_scaling=1.25
+CHULL=false
+ALS_conv=0
