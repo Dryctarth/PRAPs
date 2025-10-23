a03801d0fc6da84f808d27435b705a6793461dbc
Dryctarth <xanshriekal@yahoo.com>
2025-10-22 21:36:54 -0500

    Add files via upload

---
+++ tutorial/CMo/inpraps.sh
@@ -0,0 +1,18 @@
+#SPHt_10
+els=(C Mo)
+LevMTP=16
+DFT_CFG=aflow_CMo.cfg
+URX_CFG=randspg_CMo.cfg
+REF_CFG=ref.cfg
+mindist=0.9
+maxdist=3.1
+cmpd_pth=.../PRAPs/tutorial/CMo
+chull_var=0.05
+relax_settings="--iteration-limit=100"
+basic_acc=false
+filter_trajectories=false
+filter_volumes=false
+volume_scaling=1.25
+CHULL=false
+ALS_conv=0
+
